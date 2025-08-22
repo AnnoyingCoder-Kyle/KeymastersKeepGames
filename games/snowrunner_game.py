2b8ca3cd116c3bee66a4cb96e75b850c3d8fb3fe
AnnoyingCoder-Kyle <kyle.watt36@gmail.com>
2025-08-22 19:47:41 +1000

    starting snowrunner .py file. Not finished, errors

---
+++ games/snowrunner_game.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import functools
+from typing import List, Dict, Set
+
+from dataclasses import dataclass
+
+from Options import Toggle, OptionSet
+
+from ..game import Game
+from ..game_objective_template import GameObjectiveTemplate
+
+from ..enums import KeymastersKeepGamePlatforms
+
+
+# Option Dataclass
+@dataclass
+class TinyBookshopArchipelagoOptions:
+    pass
+
+# Main Class
+class SnowrunnerGame(Game):
+    name = "Snowrunner"
+    platform = KeymastersKeepGamePlatforms.PC
+    
+    platforms_other = [
+        KeymastersKeepGamePlatforms.
+    ]
+    
